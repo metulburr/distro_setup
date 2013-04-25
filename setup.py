aa3275ec54c0a77ee9ea4cd316db5400381d29e3
Micah <micahpage911@yahoo.com>
2013-04-25 02:34:28 -0400

    distro setup

---
+++ setup.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+import os
+import urllib.request
+import shutil
+import tarfile
+import subprocess
+
+def pygame_install(name):
+	#extract tar, directory create is just pygame
+	extract(name) 
+	#download dependencies
+	command(install_packages='python3 python3-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsdl1.2-dev libsmpeg-dev python-numpy subversion libportmidi-dev')
+	os.chdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pygame'))
+	command(cmd='sudo python3 setup.py install')
+		
+def geany_install(packages):
+	command(install_packages=packages)
+	choice = input('Use python3 for geany run command default? [y/n] ')
+	if choice.lower() == 'y':
+		fullpath = '/usr/share/geany/filetypes.python'
+		print('modifying {}'.format(fullpath))
+		
+		f = open(fullpath)
+		file_list = f.readlines()
+		for line in file_list:
+			ind = file_list.index(line)
+			if 'compiler=' in line:
+				file_list[ind] = 'compiler=python3 -m py_compile "%f"\n'
+			elif 'run_cmd' in line:
+				file_list[ind] = 'run_cmd=python3 "%f"\n'
+
+		command('sudo rm {}'.format(fullpath))
+		name = os.path.split(fullpath)[1]
+		path = os.path.split(fullpath)[0]
+		print('creating new {}'.format(name))
+		new = open(name, 'w')
+		for line in file_list:
+			new.write(line)
+		new.close()
+		command(cmd='sudo mv {} {}'.format(name, path))
+		
+			
+
+def extract(f):
+	filer = tarfile.open(f)
+	if tarfile.is_tarfile(f):
+		print('extracting {}'.format(f))
+		filer.extractall()
+
+
+	
+def command(cmd=None, install_packages=None):
+	if install_packages:
+		cmd = 'sudo apt-get install {}'.format(install_packages)
+	print(cmd)
+	proc = subprocess.Popen(cmd.split())
+	proc.wait()
+
+def download(url):
+	req = urllib.request.urlopen(url)
+	filename = os.path.split(url)[1]
+	print('downloading {}'.format(filename))
+	with open(filename,'wb') as f:
+		shutil.copyfileobj(req,f)
+	return filename
+		
+def setup(keyword, val=None):
+	print('setting up {}'.format(keyword))
+	if keyword == 'pygame':
+		pygame_install(val) #val == filename
+		print('pygame installation complete')
+	elif keyword == 'geany':
+		geany_install(val)
+		print('geany installation complete')
+	elif keyword == 'basic':
+		command(install_packages=val)
+		
+	
+packages_dict = {
+	#downloads
+	'pygame':'https://launchpad.net/debian/experimental/+source/pygame/1.9.2~pre~r3144-1/+files/pygame_1.9.2~pre~r3144.orig.tar.gz',
+	
+	#package manager installs
+	'geany':'geany geany-plugins',
+	'basic':'gparted openjdk-6-jdk openjdk-7-jdk vlc hwinfo python-dev xchat wine winetricks python-tk python3-tk k3b unetbootin tor eclipse nautilus-open-terminal libqt4-dev python-qt4 python3-pyqt4 git git-core git-gui git-doc python-pygame curl openbox obconf obmenu openbox-xdgmenu nitrogen grub-customizer mumble weechat weechat-curses terminator tmux ssh gufw gimp gmountiso deluge rtorrent nmap skype apache2 python-pip filezilla screen ghex firefox google-chrome-stable epiphany-browser steam blender desmume zsnes htop',
+}
+
+for key, val in packages_dict.items():
+	if val.startswith('http'):
+		name = download(val)
+		setup(key, name)
+	else:
+		setup(key, val)
+
+
